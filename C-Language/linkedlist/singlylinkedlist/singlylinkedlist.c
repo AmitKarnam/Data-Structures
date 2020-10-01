0a65caf8b1751d8390114c4c028dbf57e755df72
Ramjonchhen <ramjonchhen@gmail.com>
2020-10-01 17:48:42 +0545

    code of singly linked list done.

--- C-Language/linkedlist/singlylinkedlist/singlylinkedlist.c
+++ C-Language/linkedlist/singlylinkedlist/singlylinkedlist.c
@@ -17,21 +17,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node {
+struct node
+{
     int data;
-    struct node* link;
+    struct node *link;
 };
 
 // defining the head node as global
-struct node* head=NULL;
+struct node *head = NULL;
 
 // function to create a memory block for the nodes
-struct node* getNode(int x)
+struct node *getNode(int x)
 {
-    struct node* temp;
-    temp=(struct node*)malloc(sizeof(struct node));
-    temp->data=x;
-    temp->link=NULL;
+    struct node *temp;
+    temp = (struct node *)malloc(sizeof(struct node));
+    temp->data = x;
+    temp->link = NULL;
     return temp;
 }
 
@@ -40,35 +41,35 @@ void insertAtHead()
 {
     int x;
     printf("\n Enter the element for inserting at the head: ");
-    scanf("%d",&x);
+    scanf("%d", &x);
 
-    struct node* createnode=getNode(x);
-    createnode->link=head;
-    head=createnode;
+    struct node *createnode = getNode(x);
+    createnode->link = head;
+    head = createnode;
 }
 
 void insertAtTail()
 {
     int x;
     printf("\n Enter the element for inserting at the tail: ");
-    scanf("%d",&x);
+    scanf("%d", &x);
 
-    struct node* createnode=getNode(x);
+    struct node *createnode = getNode(x);
 
     // if the linked list is empty
-    if( head==NULL )
+    if (head == NULL)
     {
-        head=createnode;
+        head = createnode;
         return;
     }
 
     // traversing to the end of the linked list
-    struct node* traverse;
-    while(traverse->link!=NULL)
+    struct node *traverse;
+    while (traverse->link != NULL)
     {
-        traverse=traverse->link;
+        traverse = traverse->link;
     }
-    traverse->link=createnode;
+    traverse->link = createnode;
 }
 
 // position starts from 1 for inserting at head it is position 1
@@ -78,109 +79,109 @@ void insertAtN()
 
     int pos;
     printf("\n Enter the position of the element to insert: ");
-    scanf("%d",&pos);
+    scanf("%d", &pos);
 
     int x;
-    printf("\nEnter the element to insert at %d position: ",pos);
-    scanf("%d",&x);
+    printf("\nEnter the element to insert at %d position: ", pos);
+    scanf("%d", &x);
 
-    struct node* createnode = getNode(x);
+    struct node *createnode = getNode(x);
 
-    if(pos==1)
+    if (pos == 1)
     {
-        createnode->link=head;
-        head=createnode;
+        createnode->link = head;
+        head = createnode;
         return;
     }
 
-    struct node* traversenode=head;
+    struct node *traversenode = head;
     int i;
-    for(i=1; i<pos-1; i++)
+    for (i = 1; i < pos - 1; i++)
     {
-        traversenode=traversenode->link;
+        traversenode = traversenode->link;
     }
-    createnode->link=traversenode->link;
-    traversenode->link=createnode;
+    createnode->link = traversenode->link;
+    traversenode->link = createnode;
 }
 
 void deleteAtN()
 {
-    struct node* traversenode=head;
+    struct node *traversenode = head;
 
     displayLinkedList();
 
     int position;
     printf("\n Enter the position for deletion of node: ");
-    scanf("%d",&position);
+    scanf("%d", &position);
 
-    if(position==1)
+    if (position == 1)
     {
-        head=traversenode->link;
+        head = traversenode->link;
         free(traversenode);
         return;
     }
 
     // going to the n-1th node
     int i;
-    for(i=1; i<position-1; i++)
+    for (i = 1; i < position - 1; i++)
     {
-        traversenode=traversenode->link;
+        traversenode = traversenode->link;
     }
 
     // going to the nth node
-    struct node* traversenode1=traversenode->link;
-    traversenode->link=traversenode1->link;
-    free(traversenode1);// freeing the nth node
+    struct node *traversenode1 = traversenode->link;
+    traversenode->link = traversenode1->link;
+    free(traversenode1); // freeing the nth node
 }
 
 void displayLinkedList()
 {
     printf("\n Status of Linked List: ");
-    if( head == NULL )
+    if (head == NULL)
     {
         printf("\n Linked list is empty please enter some elements first. \n");
         return;
     }
-    struct node* traverse=head;
+    struct node *traverse = head;
     printf("\n Linked List elements are: ");
-    while(traverse!=NULL)
+    while (traverse != NULL)
     {
-        printf("%d ",traverse->data);
-        traverse=traverse->link;
+        printf("%d ", traverse->data);
+        traverse = traverse->link;
     }
     printf("\n");
 }
 
 void deleteAtHead()
 {
-    if(head==NULL)
+    if (head == NULL)
     {
         printf("\n Linked List is empty please enter something first. \n");
         return;
     }
 
-    struct node* temp=head;
-    head=head->link;
+    struct node *temp = head;
+    head = head->link;
 
     free(temp);
 }
 
 void deleteAtTail()
 {
-    struct node* temp;
-    struct node* temp1;
-    if(head == NULL)
+    struct node *temp;
+    struct node *temp1;
+    if (head == NULL)
     {
         printf("\n Linked List is empty please enter something first. \n");
         return;
     }
 
-    else if ( head->link == NULL)
+    else if (head->link == NULL)
     {
-       temp = head;
-       head = NULL;
-       printf("\n Element deleted is %d \n",temp->data);
-       free(temp);
+        temp = head;
+        head = NULL;
+        printf("\n Element deleted is %d \n", temp->data);
+        free(temp);
     }
 
     else
@@ -188,14 +189,14 @@ void deleteAtTail()
         temp = head;
         temp1 = head->link;
 
-        while( temp1->link!=NULL )
+        while (temp1->link != NULL)
         {
             temp = temp1;
-            temp1= temp1->link;
+            temp1 = temp1->link;
         }
 
         temp->link = NULL;
-        printf("\nDeleted item is: %d \n",temp1->data);
+        printf("\nDeleted item is: %d \n", temp1->data);
         free(temp1);
     }
 }
